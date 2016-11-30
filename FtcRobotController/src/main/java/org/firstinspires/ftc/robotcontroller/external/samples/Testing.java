6489775d9c59e52289652de64275b1e7ae498397
lok254 <KPitko552@tpaa.org>
2016-11-30 13:16:39 -0800

    got all motors and servos working

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/Testing.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/Testing.java
@@ -2,43 +2,39 @@
 package org.firstinspires.ftc.robotcontroller.external.samples;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-//import com.qualcomm.robotcore.eventloop.opmode.Enabled;
 
 @TeleOp(name = "Testing", group = "Concept")
-public abstract class Testing extends LinearOpMode {
-    
+public class Testing extends LinearOpMode {
+
     /* Declare OpMode members. */
     private ElapsedTime runtime = new ElapsedTime();
-    DcMotor Test;
 
-    Servo STest;
+    DcMotor Test1;
+    DcMotor Test2;
+    Servo STest1;
     Servo STest2;
 
     @Override
     public void runOpMode() {
-        telemetry.addData("Status", "Initialized");
+        telemetry.addData("Status", "Testing Initialized");
         telemetry.update();
 
         /* eg: Initialize the hardware variables. Note that the strings used here as parameters
          * to 'get' must correspond to the names assigned during the robot configuration
          * step (using the FTC Robot Controller app on the phone).
          */
-        // leftMotor  = hardwareMap.dcMotor.get("left_drive");
-        // rightMotor = hardwareMap.dcMotor.get("right_drive");
-        Test = hardwareMap.dcMotor.get("Test");
-        STest = hardwareMap.servo.get("STEST");
-        STest2 = hardwareMap.servo.get("STEST2");
+        Test1 = hardwareMap.dcMotor.get("Test1");
+        Test2 = hardwareMap.dcMotor.get("Test2");
+        STest1 = hardwareMap.servo.get("STest1");
+        STest2 = hardwareMap.servo.get("STest2");
 
         // eg: Set the drive motor directions:
         // "Reverse" the motor that runs backwards when connected directly to the battery
-        // leftMotor.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
-        // rightMotor.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+//        Test1.setDirection(DcMotor.Direction.FORWARD); // Set to FORWARD if using AndyMark motors
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -46,47 +42,24 @@ public abstract class Testing extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Status", "Testing Run Time: " + runtime.toString());
             telemetry.update();
 
             // eg: Run wheels in tank mode (note: The joystick goes negative when pushed forwards)
-            // leftMotor.setPower(-gamepad1.left_stick_y);
-            // rightMotor.setPower(-gamepad1.right_stick_y);
-            float Testy = -gamepad1.left_stick_y;
-
-            Test.setPower(Testy);
+            Test1.setPower(-gamepad1.left_stick_y);
+            Test2.setPower(-gamepad1.right_stick_y);
 
             if (gamepad1.dpad_up) {
-                STest.setPosition(0.5);
+                STest1.setPosition(0.5);
+            } else if (gamepad1.dpad_down) {
+                STest1.setPosition(-0.5);
             }
 
-            if(gamepad1.dpad_down) {
+            if (gamepad1.dpad_right) {
                 STest2.setPosition(0.5);
+            } else if (gamepad1.dpad_left) {
+                STest2.setPosition(-0.5);
             }
         }
     }
-
-//    @Override
-//    public void init() {
-//
-//        Test = hardwareMap.dcMotor.get("Test");
-//        STest = hardwareMap.servo.get("STEST");
-//        STest2 = hardwareMap.servo.get("STEST2");
-//
-//    }
-//
-//    @Override
-//    public void loop() {
-//
-//        float Testy = -gamepad1.left_stick_y;
-//
-//        Test.setPower(Testy);
-//
-//        if (gamepad1.dpad_up) {
-//            STest.setPosition(0.5); }
-//
-//        if(gamepad1.dpad_down) {
-//            STest2.setPosition(0.5);
-//        }
-//    }
 }
